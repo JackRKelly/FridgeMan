a2186a1dfaf482c5dfb42d6580cb1fff9681cdff
MyManVan <kcjackkelly@gmail.com>
2020-04-02 13:26:51 -0500

    Created page style sheets

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "./home.scss";
 
 const Home = () => {
   return (
-    <>
+    <div className="home">
       <h1>Welcome to Fridge Man</h1>
       <p>You can view your stock <a href="stocks">here</a>. You can also edit the location list <a href="locations">here</a>.</p>
-    </>
+    </div>
   );
 };
 
